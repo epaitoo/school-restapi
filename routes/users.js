6dd475bea4dd194ec3cee3f1f5872887f300ffa1
Eugene Paitoo <eugenepaitoo@gmail.com>
2019-07-02 11:49:38 +0000

    different approach for test authentication

--- routes/users.js
+++ routes/users.js
@@ -9,80 +9,54 @@ const { User } = models;
 
 
 
-
 const authenticateUser = (req, res, next) => {
-  let message = null;
+  // let message = null;
 
   // Parse the user's credentials from the Authorization header.
   const credentials = auth(req);
 
   // If the user's credentials are available...
   if (credentials) {
-    // compare emailAddress property to the user credentials name property:
-    
-    // find a user from the Users Model
-     User.findOne({
+
+    User.findOne({
       where : {
         emailAddress : credentials.name
       }
-    }).then((data) => {
-      const user = data;
-      
-       // If a user was successfully retrieved from the data store
-      // Use the bcryptjs npm package to compare the user's password
-      // (from the Authorization header) to the user's password
-      if (user) {
-        const authenticated = bcryptjs
-          .compareSync(credentials.pass, user.password);
-
-            // If the passwords match store the retrieved user object on the request object
-            // so any middleware functions that follow this middleware function will have access to the user's information.
+    })
+      .then((user) => {
+        if (user) {
+          
+            const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
           if (authenticated) {
-            console.log(`Authentication successful for email Address: ${user.emailAddress}`);
             req.currentUser = user;
+            console.log(`Authentication successful for email Address: ${user.emailAddress}`);
           } else {
-            message = `Authentication failure for user with email Address: ${user.emailAddress}`;
+            console.log(`Authentication failure for user with email Address: ${user.emailAddress}`);
           }
-      } else {
-        message = `User not found : ${credentials.name}`;
-      }
-      
-    });
+        } else {
+          console.log(`User not found : ${credentials.name}`);
+        }
 
+      });
   } else {
-    message = 'Auth header not found';
+    console.log('Auth header not found');
   }
 
-  // If user authentication failed...
-  if (message) {
-    console.warn(message);
-    // Return a response with a 401 Unauthorized HTTP status code.
-    res.status(401).json({ message: 'Access Denied' });
-
-  } else {
-    // Or if user authentication succeeded...
-    // Call the next() method.
-    next();
-  }
+ next();
 }
  
 
 
 //Returns the currently authenticated user
 router.get('/', authenticateUser, (req, res) => {
-  const user = req.currentUser;
-  res.json({
-    firstName: user.firstName,
-    lastName: user.lastName,
-  });
 
-  
-  
 });
 
 // create a new user
 router.post('/', (req, res, next) => {
   const user = req.body
+  // Hash the new user's password.
+  user.password = bcryptjs.hashSync(user.password);
   User.create({
     firstName: user.firstName,
     lastName: user.lastName,
@@ -90,8 +64,6 @@ router.post('/', (req, res, next) => {
     password: user.password
   }).then(() => {
     res.status(201).end()
-    // Hash the new user's password.
-    user.password = bcryptjs.hashSync(req.body.password);
     
   }).catch((err) => {
     err.status = 400;
